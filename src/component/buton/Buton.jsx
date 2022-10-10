af91013e573e0e5bdff872a15bbb975386452e99
MELTEM <meltemfsanaldi@gmail.com>
2022-10-11 02:41:45 +0300

    btn

---
+++ src/component/buton/Buton.jsx
@@ -0,0 +1,15 @@
+
+
+import React from 'react'
+import ButonStyle from "./buton.module.css"
+
+const Buton = () => {
+  return (
+    <div>
+        
+        <button>Surprise me</button>
+    </div>
+  )
+}
+
+export default Buton
--- src/component/card/Card.jsx
+++ src/component/card/Card.jsx
@@ -1,6 +1,12 @@
 
 import React from 'react'
+import Buton from '../buton/Buton';
 import CardStyle from "./card.module.css"
+
+
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+
+
 
 const Card = ({data}) => {
     
@@ -8,15 +14,22 @@ const Card = ({data}) => {
     <div>
         {data.map((item) => {
             const {id,image,name,job,text} = item;
-            return(
-                <div key={id} className={CardStyle.card} >
-                      <img src={image} alt="" />
-                      <h2>{name} </h2>
-                      <h3>{job} </h3>
-                       <p>{text} </p>
-                </div>
-      
-            )
+            return (
+              <div key={id} className={CardStyle.card}>
+                <img src={image} alt="" />
+                <h2>{name} </h2>
+                <h3>{job} </h3>
+                <p>{text} </p>
+                <span>
+                  <FaChevronLeft />
+                </span>
+                <span>
+                  <FaChevronRight />
+                </span>
+
+                <Buton />
+              </div>
+            );
         })}
        
     </div>
